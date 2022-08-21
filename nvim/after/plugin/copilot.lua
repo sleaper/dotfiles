0ca9a20b17879822a0bc010954fbc80bb1dd42cb
Petr Spac <petrik.spac@gmail.com>
2022-08-21 17:58:06 +0200

    Added tmux status line

---
+++ nvim/after/plugin/copilot.lua
@@ -0,0 +1,4 @@
+-- use this table to disable/enable filetypes
+vim.g.copilot_filetypes = { xml = false }
+
+vim.cmd[[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
---
+++ nvim/lua/rambo/lsp/init.lua
@@ -0,0 +1,8 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+	return
+end
+
+require("rambo.lsp.lsp-installer")
+require("rambo.lsp.handlers").setup()
+require "rambo.lsp.null-ls"
