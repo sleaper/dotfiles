44e3109f0b1349cbcad88b43acc23460cedf735c
Petr Spac <petrik.spac@gmail.com>
2023-07-19 12:47:25 +0200

    cleaning

--- nvim/lua/user/gitsigns.lua
+++ nvim/lua/user/gitsigns.lua
@@ -1,7 +1,7 @@
 local M = {
   "lewis6991/gitsigns.nvim",
   commit = "ec4742a7eebf68bec663041d359b95637242b5c3",
-  event = "BufReadPre",
+  -- event = "BufReadPre",
 }
 
 M.opts = {
--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -24,10 +24,12 @@ function M.config()
     -- ensure_installed = "all", -- one of "all" or a list of languages
     ignore_install = { "" }, -- List of parsers to ignore installing
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+    auto_install = true,
 
     highlight = {
       enable = true, -- false will disable the whole extension
       disable = { "css" }, -- list of language that will be disabled
+      additional_vim_regex_highlighting = false,
     },
     autopairs = {
       enable = true,
