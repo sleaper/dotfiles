3fc0b1db338ca3b311bd528a0e13358ef6c7b5a3
Petr Spac <petrik.spac@gmail.com>
2023-08-30 23:20:13 +0200

    remove old backup

--- backup/nvim/lua/rambo/plugins/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "rambo.plugins.lsp.mason"
-require("rambo.plugins.lsp.handlers").setup()
-require "rambo.plugins.lsp.null-ls"
