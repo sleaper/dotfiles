33df32797d67be56abb8858fee5ae8bcb70d267d
Petr Spac <petrik.spac@gmail.com>
2023-09-19 22:05:32 +0200

    feat: add null-ls for prettier

---
+++ nvim/lua/custom/plugins/prettier.lua
@@ -0,0 +1,21 @@
+return {
+	"MunifTanjim/prettier.nvim",
+	config = function()
+		local status, prettier = pcall(require, "prettier")
+		if (not status) then return end
+
+		prettier.setup {
+			bin = 'prettierd',
+			filetypes = {
+				"css",
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+				"json",
+				"scss",
+				"less"
+			}
+		}
+	end
+}
