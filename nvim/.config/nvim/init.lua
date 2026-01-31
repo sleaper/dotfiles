a48fbbcb0c1ff3297948ca988b74557fa89fd605
Petr Spac <petrik.spac@gmail.com>
2026-01-31 23:02:55 +0100

    gnu stow

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -655,7 +655,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
+    main = 'nvim-treesitter',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
